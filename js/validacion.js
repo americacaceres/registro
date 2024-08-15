1411530b4c4e25f3832be3cfb44ffa636c571438
america <americacaceres10@gmail.com>
2024-08-15 14:32:23 -0300

    verificavContraseñas iguales Co-authored-by: Avril Albistur <avrilalbistur@users.noreply.github.com>

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,26 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+
+
+function compararClave()  {
+    let password1 = document.getElementById("password1").value;
+    let password2 = document.getElementById("password2").value;
+    console.log(password1);
+    if (password1 === password2) {
+        showAlertSuccess();
+    }
+    else {
+        showAlertError();
+    
+    }
+
+}
+
+let btn = document.getElementById('regBtn');
+
+btn.addEventListener('click', () =>{
+    compararClave();
+});
+
